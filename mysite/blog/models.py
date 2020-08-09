63ff96ba5e53a72d82f35e33691eea94b899cc29
1997shp <949198124@qq.com>
2020-08-09 20:12:29 +0800

    博客内容增加富文本编辑

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from ckeditor_uploader.fields import RichTextUploadingField
 # Create your models here.
 class BlogType(models.Model):
 
@@ -19,7 +19,7 @@ class Blog(models.Model):
     blog_type = models.ForeignKey(BlogType,on_delete=models.DO_NOTHING,default=1)
 
     #内容
-    content = models.TextField()
+    content = RichTextUploadingField()
 
     #作者
     author = models.ForeignKey(User,on_delete=models.DO_NOTHING)
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from .views import home
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('',home,name = 'home'),
     path('admin/', admin.site.urls),
+    path('ckeditor', include('ckeditor_uploader.urls')),
     path('blog/',include('blog.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
